08dd1e0426f4051dec9aaecb3ada242ba3e26ca9
dbsrjs <dbsrjs1224@gmail.com>
2024-06-01 00:06:30 +0900

    06/01 save

--- Assets/0.Scripts/Bullet.cs
+++ Assets/0.Scripts/Bullet.cs
@@ -6,10 +6,11 @@ public class Bullet : MonoBehaviour
 {
     public int damage;
     public bool isMelee;
+    public bool isRock;
 
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.gameObject.tag == "Floor")
+        if(collision.gameObject.tag == "Floor" && !isRock)
         {
             Destroy(gameObject, 3); //3초 뒤에 삭제
         }
